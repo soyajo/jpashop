134b99f3d8103d4611dfa4c512cab793a482f696
josoya <thdi4564@nate.com>
2023-03-09 10:53:58 +0900

    testcase 생성

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
 
+		Hello hello = new Hello();
+		hello.setData("hello");
+		System.out.println("hello.getData() = " + hello.getData());
+		SpringApplication.run(JpashopApplication.class, args);
+
+	}
 }
