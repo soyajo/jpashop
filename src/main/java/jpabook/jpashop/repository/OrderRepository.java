1571859143459426a1ae2b0a7ed2ddff67fcb746
josoya <thdi4564@nate.com>
2023-03-16 09:08:14 +0900

    querydsl 사용

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -9,7 +9,6 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -103,12 +102,14 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+
     public List<Order> findAll(OrderSearch orderSearch) {
         return query
                 .select(order)
                 .from(order)
                 .join(order.member, member)
                 .where(statusEq(orderSearch.getOrderStatus()),nameLike(orderSearch.getMemberName()))
+//                .where()
                 .limit(1000)
                 .fetch();
     }
